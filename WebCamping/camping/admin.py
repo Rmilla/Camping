16ecaf0d52830856e65a7a5c43779aabb3a67f8f
Rmilla <robin.milla.pro@gmail.com>
2024-03-29 14:10:31 +0100

    modifications de admin.py

--- WebCamping/camping/admin.py
+++ WebCamping/camping/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .. import models
 # Register your models here.
+
+admin.site.register(models.Camping)
+admin.site.register(models.Client)
+admin.site.register(models.Adresse)
+admin.site.register(models.Manager_camping)
