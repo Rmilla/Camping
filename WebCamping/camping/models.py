b7e0e1c3b160a3bf8fe68157fb131307e136d990
Rmilla <robin.milla.pro@gmail.com>
2024-04-02 17:26:29 +0200

    modifications modèles et fonctions calcul emission

--- WebCamping/camping/models.py
+++ WebCamping/camping/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from mongoengine import Document, fields
-# Create your models here.
+# Créations des modèles.
 
 class Camping(Document):
+    id_camping = fields.StringField(required=True, max_length=200)
     id_Adresses = fields.StringField(required=True, max_length=200)
     nom = fields.StringField(required=True, max_length=200)
     code_postale = fields.StringField(required=True)
@@ -18,6 +19,7 @@ class Adresses_campings(Document):
     Pays = fields.StringField(required=True, max_length=200)
 
 class Client(Document):
+    id_client = fields.StringField(required=True, max_length=200)
     adress = fields.StringField(required=True, max_length=200)
     code_postal = fields.StringField(required=True, max_length=200)
     ville = fields.StringField(required=True, max_length=200)
@@ -25,3 +27,31 @@ class Client(Document):
     longitude = fields.StringField(required=True, max_length=200)
     latitude = fields.StringField(required=True, max_length=200)
 
+class Voyager(Document):
+    emission = fields.models.FloatField(required=True)
+    vehicule = fields.StringField(required=True, choices=[('voiture','Voiture'),('avions','Avions'),('train','Train'),('moto','Moto'),('voitures_e','Voitures_electrique')], default='voiture')
+    distance_parcourue = fields.models.FloatField(required=True)
+    id_client = fields.StringField(required=True, max_length=200)
+    id_camping = fields.StringField(required=True, max_length=200)
+
+
+def calcul_emission(Voyager):
+    # définis les facteurs d'émissions
+    facteurs = {
+        'voiture': 0.218,
+        'moto': 0.191,
+        'avions': 0.188,
+        'voitures_e': 0.103,
+        'train': 0.003
+    }
+
+    # vérifie si le vehicule est bien dans la liste des facteurs
+    if Voyager.vehicule not in facteurs:
+        raise ValueError(f"Vehicule type '{Voyager.vehicule}' is not supported.")
+
+    # Calcule le taux d'émissions
+    Voyager.emission = Voyager.distance_parcourue * facteurs[Voyager.vehicule]
+
+    return Voyager.emission
+
+print(f"The emission for a {Voyager.vehicule} traveling {Voyager.distance_parcourue} km is {Voyager.emission} kg of CO2.")
