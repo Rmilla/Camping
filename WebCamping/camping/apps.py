5fc6040417beb54fbdf0f10a8ade38020c430896
Pierre Sabat <sabat.pierre@gmail.com>
2024-04-02 14:50:10 +0200

    apps.py avec la seconde méthode de connexion

--- WebCamping/camping/apps.py
+++ WebCamping/camping/apps.py
@@ -10,19 +10,19 @@ env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 HOST=env("MONGODB_HOST")
-PORT=env("MONGODB_PORT")
+PORT=int(env("MONGODB_PORT"))
 DATABASE="TestCamping"
 PASSWORD=env("MONGODB_PASSWORD")
 USERNAME=env("MONGODB_USERNAME")
 
 #A voir si il faut vraiment rajouter le port (PORT) pour obtenir la connection
-uri=f"mongodb+srv://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}"
+uri=f"mongodb+srv://{USERNAME}:{PASSWORD}@{HOST}/{DATABASE}"
 print(uri)
 
 class CampingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'camping'
-
+connect(db=DATABASE, username=USERNAME,password=PASSWORD,host=HOST,port=PORT)
 """    def ready(self):
         # Define the MongoDB connection settings
         # Connect to MongoDB
