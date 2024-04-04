3115a05ea94fa964bebd7b721c64c5fc56210fb2
Rmilla <robin.milla.pro@gmail.com>
2024-04-04 17:31:23 +0200

    change settings

--- WebCamping/WebCamping/settings.py
+++ WebCamping/WebCamping/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'django-mongoengine'
+    'django-mongoengine',
     'django-mongoengine.mongo_auth',
     'django_mongoengine.mongo_admin',
     "rest_framework.authtoken",
