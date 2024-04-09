15b301cb31c7286b69204fbdff087c52cf44f4b9
Rmilla <robin.milla.pro@gmail.com>
2024-04-09 10:23:35 +0200

    changement models, import

--- WebCamping/camping/management/commands/import_csv.py
+++ WebCamping/camping/management/commands/import_csv.py
@@ -3,27 +3,15 @@ import requests
 import googlemaps
 import csv
 import itertools
-
 class Command(BaseCommand):
         # remplir avec votre clé API Google Maps
-        gmaps = googlemaps.Client(key='AIzaSyBawSSjukicDdh7sfbVcToVktSypmWwQmk')
+        #gmaps = googlemaps.Client(key='AIzaSyBawSSjukicDdh7sfbVcToVktSypmWwQmk')
         csv_file_path ='Fakedata.csv'
         #def add_arguments(self, parser):
         #    parser.add_argument(csv_file_path, type=str, help='Path to the CSV file')
         
         def handle(self,*args,**kwargs):
-            
-            def read_line_csv(csv_file_path, begin, end ):
-                with open(csv_file_path, 'r', newline='', encoding='ISO-8859-1') as file:
-                    reader = csv.DictReader(file, delimiter=';')
-                    for _ in range(begin):
-                        next(reader)
-                    return list(itertools.islice(reader, end-begin))
-            for line in range(0, 1001, 100):
-                part1 = read_line_csv(self.csv_file_path, 1, 100)
-                print(part1)
-            
-            """""
+            #csv_file_path =kwargs[Fakedata.csv]
             print(self.csv_file_path)
             def read_parts(csv_file_path):
                 with open(csv_file_path,'r',encoding='ISO-8859-1') as file:
@@ -34,12 +22,12 @@ class Command(BaseCommand):
                         print(row)
                         i=i+1
             read_parts(self.csv_file_path)
-            """
-        directions_result = gmaps.directions("Sydney Town Hall",
-                                            "Parramatta, NSW",
-                                            mode="driving",)
+        
+        #directions_result = gmaps.directions("Sydney Town Hall",
+        #                                    "Parramatta, NSW",
+        #                                    mode="driving",)
         #stock la distance dans une variable.
-        distance = directions_result[0]['legs'][0]['distance']['text']
+        #distance = directions_result[0]['legs'][0]['distance']['text']
         #afficher le résultat
         #print(distance)
 
