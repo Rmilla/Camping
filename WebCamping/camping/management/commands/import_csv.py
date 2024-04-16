c89e385332e5e348e1939ade3d054c9d52facf67
Rmilla <robin.milla.pro@gmail.com>
2024-04-16 15:15:59 +0200

    modifications import_csv

--- WebCamping/camping/management/commands/import_csv.py
+++ WebCamping/camping/management/commands/import_csv.py
@@ -1,34 +1,44 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 import requests
-import googlemaps
 import csv
 import itertools
+import os
+import json
 import mongoengine
 from camping.models import Flat, Camping, Client, Trip
 
 
+'''
+PARTIE1
+CODE1
+
+PARTIE2
+CODE2
+'''
+
 class Command(BaseCommand):
-    mongoengine.connect(db="TestCamping", host=f"mongodb+srv://cluster0.l5yaw7u.mongodb.net/",
-                    username='camping', password="SqP6B8wLx62DsUf6", alias='import')
-    # remplir avec votre clé API Google Maps
-    API_KEY = "AIzaSyBawSSjukicDdh7sfbVcToVktSypmWwQmk"
-    # gmaps = googlemaps.Client(key='AIzaSyBawSSjukicDdh7sfbVcToVktSypmWwQmk')
+    def __init__(self):
+        self.connect_mongodb()
+
+    def connect_mongodb(self):
+        if not mongoengine.connection._connections:
+            mongoengine.connect(db="TestCamping", host=f"mongodb+srv://cluster0.l5yaw7u.mongodb.net/", username='camping', password="SqP6B8wLx62DsUf6", alias='default')
+        else:
+            mongoengine.connection.disconnect()
 
     def add_arguments(self, parser):
         parser.add_argument("API_KEY", type=str, help="API Key for Google Maps")
 
-    def get_dist(self, api_key, start, end, mode):
+    def get_distance(self, api_key, start, end, mode):
         base_url = "https://maps.googleapis.com/maps/api/distancematrix/json"
         params = {"origins": start, "destinations": end, "mode": mode, "key": api_key}
         response = requests.get(base_url, params=params)
         if response.status_code == 200:
             data = response.json()
-            # print(f"Type des données renvoyées : {type(data)}")
-            # print(f"Données brutes : {data}")
+            print("data", data)
             if data["status"] == "OK":
-                #print(data["rows"][0]["elements"][0])
-                if data["rows"][0]["elements"][0] == {'status' : 'NOT_FOUND'} or data["rows"][0]["elements"][0] == {'status' : 'ZERO_RESULTS'}:
-                    return "PROBLEM" 
+                if data["rows"][0]["elements"][0] in ({'status': 'NOT_FOUND'}, {'status': 'ZERO_RESULTS'}):
+                    return "PROBLEM"
                 else:
                     distance = data["rows"][0]["elements"][0]["distance"]["text"].split(" ")[0]
                     return distance
@@ -41,115 +51,90 @@ class Command(BaseCommand):
             print(response)
             return 'PROBLEM'
 
-    csv_file_path = "Fakedata.csv"
+    def read_csv_lines(self, csv_file_path, begin, end):
+        with open(csv_file_path, "r", newline="", encoding="ISO-8859-1") as file:
+            reader = csv.DictReader(file, delimiter=";")
+            for _ in range(begin):
+                next(reader)
+            return list(itertools.islice(reader, end - begin))
 
     def handle(self, *args, **options):
-        API_KEY = "AIzaSyBawSSjukicDdh7sfbVcToVktSypmWwQmk"
+        API_KEY = options["API_KEY"]
 
-        def read_line_csv(csv_file_path, begin, end):
-            with open(csv_file_path, "r", newline="", encoding="ISO-8859-1") as file:
-                reader = csv.DictReader(file, delimiter=";")
-                for _ in range(begin):
-                    next(reader)
-                return list(itertools.islice(reader, end - begin))
-        j=1
-        for line in range(0, 903, 100):
-            part = read_line_csv(self.csv_file_path, line, line + 100)
-            for i in range(0, 100):
-                print(f"Enregistrement {j}")
-                j=j+1
-                name_camping = part[i]["camping"].split(sep="/")[0]
-                camping_postal_adress = part[i]["camping"].split(sep="/")[1]
-                camping_city = part[i]["camping"].split(sep="/")[2]
-                country_camping = part[i]["camping"].split(sep="/")[3]
-                country_client = part[i]["Country"]
-                city_client = part[i]["City"].split(sep=" ")[0]
-                year = part[i]["Year"]
-                if part[i]["Transport"] == "Train":
-                    APItransport = "transit"
-                    distance = self.get_dist(
-                        API_KEY, city_client, camping_city, APItransport)
-                    if distance == 'PROBLEM':
-                        continue
-                    elif "," in distance:
-                        distance = float(distance.replace(",", "."))*1000
-                    else:
-                        distance = float(distance)
-                    transport_em = 0.003
-                    emission_total = transport_em * distance
-                elif part[i]["Transport"] == "Electric engine car":
-                    APItransport = "driving"
-                    transport_em = 0.103
-                    distance = self.get_dist(
-                        API_KEY, city_client, camping_city, APItransport)
-                    if distance == 'PROBLEM':
-                        continue                
-                    elif "," in distance:
-                        distance = float(distance.replace(",", "."))*1000
-                    else:
-                        distance = float(distance)
-                    emission_total = transport_em * distance
-                elif part[i]["Transport"] == "Combustion engine car":
-                    APItransport = "driving"
-                    distance = self.get_dist(
-                        API_KEY, city_client, camping_city, APItransport
-                    )
-                    if distance == 'PROBLEM':
-                        continue
-                    elif "," in distance:
-                        distance = float(distance.replace(",", "."))*1000
-                    else:
-                        distance = float(distance)
-                    transport_em = 0.218
-                    emission_total = transport_em * distance
-                else:
-                    APItransport == "driving"
-                    distance = self.get_dist(
-                        API_KEY, city_client, camping_city, APItransport
-                    )
-                    if distance == 'PROBLEM':
-                        continue
-                    elif "," in distance:
-                        distance = float(distance.replace(",", "."))*1000
-                    else:
-                        distance = float(distance)
-                    transport_em = 0.113
-                    emission_total = transport_em * distance
-                print(f"Nom du camping : {name_camping}, Ville du camping : {camping_city}, Ville du client : {city_client}, Année : {year} , Mode de Transport : {part[i]['Transport']}")
-                print(f"Distance : {distance}, Type de la distance : {type(distance)}")
-                row_object = Flat(
-                    name_camping=name_camping,
-                    adress_camping=camping_postal_adress,
-                    city=camping_city,
-                    country_camping=country_camping,
-                    client_country=country_client,
-                    client_city=city_client,
-                    year=year,
-                    transport_mode=part[i]["Transport"],
-                    distance=distance,
-                    emission_total=emission_total,
-                )
-                row_object.save()
-                
-                camping_object = Camping(
-                    camping_name = name_camping,
-                    camping_city = camping_city,
-                    camping_country = country_camping,
-                )
-                camping_object.save()
-                client_object= Client(
-                    client_city = city_client,
-                    client_country = country_client,
-                )                
-                client_object.save()
-                trip_object = Trip(
-                    emissions = emission_total,
-                    vehicle = part[i]["Transport"],
-                    distance = distance,
-                    year = year,
-                )
-                trip_object.save()
-            mongoengine.disconnect(alias='import')    
+        vehicle_emissions = self.load_vehicle_data()
+
+        step = 100
+        for line in range(0, 1000, step):
+            part = self.read_csv_lines("Fakedata.csv", line, line + step)
+            for record in part:
+                name_camping, camping_postal_adress, camping_city, country_camping = record["camping"].split(sep="/")
+                country_client = record["Country"]
+                city_client = record["City"].split(sep=" ")[0]
+                year = record["Year"]
+                transport = record["Transport"]
+
+                API_transport = "transit" if transport == "Train" else "driving"
+                if transport not in ["Train", "Electric engine car", "Combustion engine car"]:
+                    API_transport = "driving"
+
+                distance = self.get_distance(API_KEY, city_client, camping_city, API_transport)
+                if distance == 'PROBLEM':
+                    continue
+                distance = float(distance.replace(",", ".")) * 1000
+                transport_em = vehicle_emissions.get(transport, 0)
+
+                emission_total = transport_em * distance
+
+                self.create_flat_object(name_camping, camping_postal_adress, camping_city, country_camping, country_client, city_client, year, transport, distance, emission_total)
+                self.create_camping_object(name_camping, camping_city, country_camping)
+                self.create_client_object(city_client, country_client)
+                self.create_trip_object(emission_total, transport, distance, year)
+
+    def load_vehicle_data(self):
+        json_file_path = os.path.join(os.path.dirname(__file__), 'vehicle_emissions.json')
+        with open(json_file_path, 'r') as file:
+            data = json.load(file)
+        return data
+
+    def create_flat_object(self, name_camping, camping_postal_adress, camping_city, country_camping, country_client, city_client, year, transport, distance, emission_total):
+        flat_object = Flat(
+            name_camping=name_camping,
+            adress_camping=camping_postal_adress,
+            city=camping_city,
+            country_camping=country_camping,
+            client_country=country_client,
+            client_city=city_client,
+            year=year,
+            transport_mode=transport,
+            distance=distance,
+            emission_total=emission_total,
+        )
+        flat_object.save()
+
+    def create_camping_object(self, name_camping, camping_city, country_camping):
+        camping_object = Camping(
+            camping_name=name_camping,
+            camping_city=camping_city,
+            camping_country=country_camping,
+        )
+        camping_object.save()
+
+    def create_client_object(self, city_client, country_client):
+        client_object = Client(
+            client_city=city_client,
+            client_country=country_client,
+        )
+        client_object.save()
+
+    def create_trip_object(self, emission_total, transport, distance, year):
+        trip_object = Trip(
+            emissions=emission_total,
+            vehicle=transport,
+            distance=distance,
+            year=year,
+        )
+        trip_object.save()
+
         # directions_result = gmaps.directions("Sydney Town Hall",
         #                                     "Parramatta, NSW",
         #                                     mode="driving",)
