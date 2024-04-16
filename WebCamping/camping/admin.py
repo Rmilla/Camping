397d145c977334d956213bd91e70c0c498aed0b6
Rmilla <robin.milla.pro@gmail.com>
2024-04-16 10:37:15 +0200

    modification admin

--- WebCamping/camping/admin.py
+++ WebCamping/camping/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from django_mongoengine import mongo_admin as admin
-from camping.models import Camping, Client, Flat
+from camping.models import Camping, Flat
 
 # Register your models here.
 admin.site.register(Flat)
-admin.site.register(Camping)
-admin.site.register(Client)
