b9b1dcb33131bf81a7fdb3bb532d32f9ffbe99ba
Pierre Sabat <sabat.pierre@gmail.com>
2024-04-24 12:19:19 +0200

    commenting supposed dead code from serializers

--- WebCamping/camping/serializer/client_serializer.py
+++ WebCamping/camping/serializer/client_serializer.py
@@ -6,11 +6,11 @@ class ClientSerializer(serializers.ModelSerializer):
         model = Client
         fields = '__all__'
 
-    def create(self, validated_data):
-        return Client.objects.create(**validated_data)
+    # def create(self, validated_data):
+    #     return Client.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
-        for attr, value in validated_data.items():
-            setattr(instance, attr, value)
-        instance.save()
-        return instance
+    # def update(self, instance, validated_data):
+    #     for attr, value in validated_data.items():
+    #         setattr(instance, attr, value)
+    #     instance.save()
+    #     return instance
