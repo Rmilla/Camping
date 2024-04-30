8ea38b2769cc81ea7868ac775a1900a818f347f6
Pierre Sabat <sabat.pierre@gmail.com>
2024-04-30 09:50:02 +0200

    general dasboard serializer and view available!

--- WebCamping/camping/serializer/gen_dashboard_emission_group_serializer_years.py
+++ WebCamping/camping/serializer/gen_dashboard_emission_group_serializer_years.py
@@ -1,17 +1,17 @@
 from rest_framework import serializers
 
-class General_emission_group_serializer_years(serializers.ModelSerializer):
-    class Meta:
-        fields = [
-            "2014",
-            "2015",
-            "2016",
-            "2017",
-            "2018",
-            "2019",
-            "2020",
-            "2021",
-            "2022",
-            "2023",
-            "2024",
-        ]
+class General_emission_group_serializer_years(serializers.Serializer):
+            y2013 = serializers.FloatField()
+            y2014 = serializers.FloatField()
+            y2015 = serializers.FloatField()
+            y2016 = serializers.FloatField()
+            y2017 = serializers.FloatField()
+            y2018 = serializers.FloatField()
+            y2019 = serializers.FloatField()
+            y2020 = serializers.FloatField()
+            y2021 = serializers.FloatField()
+            y2022 = serializers.FloatField()
+            y2023 = serializers.FloatField()
+
+            class Meta:
+                fields = ['y2013','y2014','y2015','y2016','y2017','y2018','y2019','y2020','y2021','y2022','y2023']
--- WebCamping/camping/views/gen_dashboard_emissions_group_view.py
+++ WebCamping/camping/views/gen_dashboard_emissions_group_view.py
@@ -2,17 +2,26 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from ..models import Camping, Client, Trip
 from ..serializer import CampingSerializer, ClientSerializer, TripSerializer
-from django.db.models import Prefetch
+from ..serializer import General_emission_group_serializer_years
+from django.db import connection
 
-class NaturalJoinView(APIView):
+class EmmissionGroup(APIView):
     def get(self, request):
-        # Prefetch related objects for Camping and Client
-        campings = Camping.objects.prefetch_related(
-            Prefetch('trip', queryset=Trip.objects.select_related('client'))
-        )
-
+        results={}
+        for year in range(2013,2024,1):
+            with connection.cursor() as cursor:
+                    cursor.execute("SELECT SUM(emissions) FROM camping_trip WHERE year = (%s)",
+                    [year],
+                    )
+                    row = cursor.fetchone()
+                    emissions = row[0]
+                    results[f'y{year}']=emissions
+        print(results)
         # Serialize the queryset
-        serializer = CampingSerializer(campings, many=True)
-
+        serializer = General_emission_group_serializer_years(data=results, many=False)
+        print(serializer)
         # Return the serialized data
-        return Response(serializer.data)
+        if serializer.is_valid():
+             return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=400)
