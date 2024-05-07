bca6c22dce9aba9aa425807763d8ba8cc359f56a
Pierre Sabat <sabat.pierre@gmail.com>
2024-05-07 10:55:25 +0200

    test for yellow files in vscode

--- WebCamping/camping/views/gen_dashboard_emissions_group_view.py
+++ WebCamping/camping/views/gen_dashboard_emissions_group_view.py
@@ -4,24 +4,40 @@ from ..models import Camping, Client, Trip
 from ..serializer import CampingSerializer, ClientSerializer, TripSerializer
 from ..serializer import General_emission_group_serializer_years
 from django.db import connection
+from ..models.login import Login
+
+class IsAdminUser(BasePermission):
+    """
+    Allow access only to Admin users.
+    """
+    def has_permission(self,request,view):
+        user = request.user
+        return user.role == Login.UserRole.ADMIN
 
 class EmmissionGroup(APIView):
+    permission_classes = [IsAdminUser]
+    
     def get(self, request):
-        results={}
-        for year in range(2013,2024,1):
-            with connection.cursor() as cursor:
-                    cursor.execute("SELECT SUM(emissions) FROM camping_trip WHERE year = (%s)",
-                    [year],
-                    )
-                    row = cursor.fetchone()
-                    emissions = row[0]
-                    results[f'y{year}']=emissions
-        print(results)
-        # Serialize the queryset
-        serializer = General_emission_group_serializer_years(data=results, many=False)
-        print(serializer)
-        # Return the serialized data
-        if serializer.is_valid():
-             return Response(serializer.data)
+        
+        if request.user.role == Login.UserRole.ADMIN:
+
+            results={}
+            for year in range(2013,2024,1):
+                with connection.cursor() as cursor:
+                        cursor.execute("SELECT SUM(emissions) FROM camping_trip WHERE year = (%s)",
+                        [year],
+                        )
+                        row = cursor.fetchone()
+                        emissions = row[0]
+                        results[f'y{year}']=emissions
+            print(results)
+            # Serialize the queryset
+            serializer = General_emission_group_serializer_years(data=results, many=False)
+            print(serializer)
+            # Return the serialized data
+            if serializer.is_valid():
+                return Response(serializer.data)
+            else:
+                return Response(serializer.errors, status=400)
         else:
-            return Response(serializer.errors, status=400)
+            return Response({"message": "Accessible only by admins."})
