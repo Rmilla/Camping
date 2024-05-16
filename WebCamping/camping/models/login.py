b9cb2037dbc556e735b53c8fe866fb10ccd31502
Rmilla <robin.milla.pro@gmail.com>
2024-05-16 16:58:59 +0200

    changement modèle login

--- WebCamping/camping/models/login.py
+++ WebCamping/camping/models/login.py
@@ -1,12 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
-
-class UserRole(models.TextChoices):
-    MANGEN = 'MG' , 'Manager général'
-    GETCAM = 'GC' , 'Gestionnaire de camping'
+import json
+import os
 
 class LoginManager(BaseUserManager):
-    use_in_migrations = True
     """ Personnalisation de la gestion des utilisateurs afin d'intégrer la gestion de l'authentification par token"""
     def create_user(self, username, password, **extra_fields):
         if not username:
@@ -19,13 +16,12 @@ class LoginManager(BaseUserManager):
 class Login(AbstractBaseUser, PermissionsMixin):
     # Appliquer la gestion particulière de l'utilisateur au model customisé, qui hérite du model de django.
     username = models.CharField(max_length=255, unique=True)
-    password = models.CharField(max_length=255)
+    password = models.CharField(max_length=20)
     administrateur = models.BooleanField(default=False)
     objects = LoginManager()
-    is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
+
     USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['password']
+    REQUIRED_FIELDS = ['username','password']
 
 
     def __str__(self):
