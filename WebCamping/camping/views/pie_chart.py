d0f2d0ea0ec42d08ae2c32b422b31e711e011cf6
Pierre Sabat <sabat.pierre@gmail.com>
2024-05-22 16:15:59 +0200

    add pie chart to views that need authentication

--- WebCamping/camping/views/pie_chart.py
+++ WebCamping/camping/views/pie_chart.py
@@ -1,11 +1,15 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from ..models import Camping, Client, Trip
 from ..serializer import CampingSerializer, ClientSerializer, TripSerializer
 from ..serializer import Pie_chart_serializer
 from django.db import connection
 
 class Pie_chart(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
     def get(self, request):
         results={}
         print("Type de la requête : ", type(request.data), "Requête : ", request.data)
